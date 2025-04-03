d8a2a708f61f22c43619e81f86f237405bc61827
Christos-A <christosari003@gmail.com>
2025-04-03 16:44:03 +0300

    Added the db connection so that everyone can start test it and let me know

---
+++ php/config.php
@@ -0,0 +1,22 @@
+<?php
+$host = 'websrv1.cut.ac.cy';          // Or the internal IP, if required
+$db   = 'cei326omada2';
+$user = 'cei326omada2user';
+$pass = 'Vp5!2BNFh!cHiN!U';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    // echo "Connected to database successfully!";
+} catch (\PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
